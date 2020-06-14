cbd9c4b881d6c23c221c8fce109e578cbd2f995f
Lê Hữu Việt Anh <lehuuvietanh@gmail.com>
2020-06-14 20:11:51 +0700

    Update saga

--- src/store/helpers/sagaHelpers.js
+++ src/store/helpers/sagaHelpers.js
@@ -1,36 +1,39 @@
-import { take, put, call } from "redux-saga/effects";
+import { put, call, takeEvery, takeLatest } from "redux-saga/effects";
 import { loadingActions } from "store/ducks/LoadingDuck";
 
-const sagaWrapper = ({ actionType, worker }) =>
-  function* watcher() {
-    while (true) {
-      const meta = {
-        data: null,
-        err: null,
-      };
-      const setMetaData = (data) => {
-        meta.data = data;
-      };
-      const setMetaErr = (err) => {
-        meta.err = err;
-      };
-
-      const action = yield take(actionType);
-      const isLoading = action.meta.isLoading;
-      if (isLoading) {
-        yield put(loadingActions.show());
-      }
-      yield call(worker, {
-        payload: action.payload,
-        action,
-        setMetaData,
-        setMetaErr,
-      });
-      console.log(meta);
-      if (isLoading) {
-        yield put(loadingActions.hide());
-      }
-    }
+function* workerRegistation(worker, action) {
+  const meta = {
+    data: null,
+    err: null,
   };
+  const setMetaData = (data) => {
+    meta.data = data;
+  };
+  const setMetaErr = (err) => {
+    meta.err = err;
+  };
+  const isLoading = action.meta.isLoading;
+  if (isLoading) {
+    yield put(loadingActions.show());
+  }
+  yield call(worker, {
+    payload: action.payload,
+    action,
+    setMetaData,
+    setMetaErr,
+  });
+  if (isLoading) {
+    yield put(loadingActions.hide());
+  }
+}
+export const registerEvery = ({ actionType, worker }) => {
+  return function* watcher() {
+    yield takeEvery(actionType, workerRegistation, worker);
+  };
+};
 
-export default sagaWrapper;
+export const registerLatest = ({ actionType, worker }) => {
+  return function* watcher() {
+    yield takeLatest(actionType, workerRegistation, worker);
+  };
+};
